140d3cfe2a5519d3624d161fd4ec6797de1bb63b
chahyunwoo <chahyunwoobi@gmail.com>
2025-03-27 13:10:04 +0900

    robot.txt, sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,42 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://chahyunwoo.dev",
+      lastModified: new Date().toISOString(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://chahyunwoo.dev/about/ko",
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://chahyunwoo.dev/about/en",
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://chahyunwoo.dev/about/jp",
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://chahyunwoo.dev/?category=Frontend",
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://chahyunwoo.dev/?tag=Next.js&parentCategory=Frontend",
+      lastModified: new Date().toISOString(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
+}
