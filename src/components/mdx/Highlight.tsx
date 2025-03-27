4fa824b2164077216d35020c25fb9663eafd6d2e
chahyunwoo <chahyunwoobi@gmail.com>
2025-03-27 23:39:22 +0900

    add mdx custom  Highlight

---
+++ src/components/mdx/Highlight.tsx
@@ -0,0 +1,3 @@
+export function Highlight({ children }: { children: React.ReactNode }) {
+  return <strong className="text-fuchsia-500">{children}</strong>;
+}
--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -3,11 +3,13 @@ import { MdxImage } from "./mdx-image";
 import { MdxLink } from "./mdx-link";
 import { Icon } from "./icon";
 import { CodeBlock } from "./code-block";
+import { Highlight } from "./Highlight";
 
 export const MdxComponents = {
   Callout,
   MdxImage,
   Icon,
   a: MdxLink,
+  Highlight,
   pre: (props: any) => <CodeBlock {...props} />,
 };
