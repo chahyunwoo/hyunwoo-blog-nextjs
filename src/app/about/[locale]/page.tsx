e3cab54f35e6d38e0c25dd1ae326f572d697b85a
chahyunwoo <chahyunwoobi@gmail.com>
2025-03-29 03:42:15 +0900

    about-page word break-keep / TODO: navigator categories componentization

--- src/app/about/[locale]/page.tsx
+++ src/app/about/[locale]/page.tsx
@@ -43,7 +43,7 @@ export default async function Page({ params }: Params<{ locale: Locale }>) {
 
   return (
     <InnerContainer className="py-12">
-      <div className="max-w-2xl mx-auto">
+      <div className="max-w-2xl mx-auto break-keep">
         <ProfileHeader profile={profile} />
 
         {/* 소개글 */}
