84f868126febde9a52c85fae9539b6443a7f42ee
chahyunwoo <chahyunwoobi@gmail.com>
2025-04-01 13:21:28 +0900

    desktop menu link style

--- src/components/features/navigation/desktop-menu.tsx
+++ src/components/features/navigation/desktop-menu.tsx
@@ -9,7 +9,12 @@ export default function DesktopMenu({ items }: DesktopMenuProps) {
   return (
     <nav className="hidden md:flex items-center gap-6">
       {items.map(({ name, href }) => (
-        <ActiveLink key={name} href={href} title={name} />
+        <ActiveLink
+          key={name}
+          href={href}
+          title={name}
+          className="hover:bg-transparent"
+        />
       ))}
     </nav>
   );
