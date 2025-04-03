24fc8296cce4b76eb3512fef58a17aef2d74a6b8
chahyunwoo <chahyunwoobi@gmail.com>
2025-04-04 08:30:32 +0900

    mdx highlight color variants

--- src/components/mdx/Highlight.tsx
+++ src/components/mdx/Highlight.tsx
@@ -1,4 +1,16 @@
-// TODO: 색상 variant 추가
-export function Highlight({ children }: { children: React.ReactNode }) {
-  return <strong className="text-fuchsia-500">{children}</strong>;
+interface HighlightProps {
+  children: React.ReactNode;
+  color?: keyof typeof colors;
+}
+
+const colors = {
+  fuchsia: "text-fuchsia-500",
+  blue: "text-blue-500",
+  green: "text-green-500",
+  yellow: "text-yellow-500",
+  red: "text-red-500",
+} as const;
+
+export function Highlight({ children, color = "fuchsia" }: HighlightProps) {
+  return <strong className={colors[color]}>{children}</strong>;
 }
